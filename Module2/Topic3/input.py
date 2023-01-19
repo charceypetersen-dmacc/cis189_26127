a3e579e08730f4adc564d68dbff092f31143f9f3
charceypetersen-dmacc <122238082+charceypetersen-dmacc@users.noreply.github.com>
2023-01-19 03:28:22 -0600

    Create input.py

---
+++ Module2/Topic3/input.py
@@ -0,0 +1,24 @@
+# asks user, nothing happens, need a variable
+# input("What is your favorite color?")
+
+# color = input("What is your favorite color?")
+# print(color)
+
+# anything coming from input comes as a string
+# age = input("What is your age?")
+# print(age)
+# print(type(age))
+
+# if you don't want your input as a string, CAST!
+# age = int(input("What is your age?"))
+# print(age)
+# print(type(age))
+
+# if you don't want your input as a string, CAST! but not fool proof - we will get to it!
+# age = int(input("What is your age?"))
+# print(age)
+# print(type(age))
+
+# num = int(input("guess a number"))
+# num2 = int(input("guess another number"))
+# print(num + num2)
