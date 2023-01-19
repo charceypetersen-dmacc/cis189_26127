a8b0794455974f42d261550243a7e60e682b5e76
charceypetersen-dmacc <122238082+charceypetersen-dmacc@users.noreply.github.com>
2023-01-19 03:27:00 -0600

    Create data_types_and_casting.py

---
+++ Module2/Topic1/data_types_and_casting.py
@@ -0,0 +1,211 @@
+# string
+# a_string = "Python is cool 758329758934 ?<>@#$%&&&^%$[]\."
+# print(a_string)
+# print(type(a_string))
+
+# integer
+# an_int = -23478932
+# print(an_int)
+# print(type(an_int))
+
+# float
+# a_float = 45.74589435
+# print(a_float)
+# print(type(a_float))
+
+# boolean
+# a_true = True
+# a_false = False
+# print(a_true)
+# print(type(a_true))
+
+# none
+# a_none = None
+# print(a_none)
+# print(type(a_none))
+
+# str to str
+# a_str = "Python"
+# a_str = str(a_str)
+# print(a_str)
+# print(type(a_str))
+
+# str to int
+
+# # does not work!!
+# a_str = "Python"
+# a_int = int(a_str)
+# print(a_int)
+# print(type(a_int))
+# a_str = "4.0"
+# a_int = int(a_str)
+# print(a_int)
+# print(type(a_int))
+
+# # does work
+# a_str = "4"
+# a_int = int(a_str)
+# print(a_int)
+# print(type(a_int))
+
+# str to float
+# # does not work
+# a_str = "four point oh"
+# a_float = float(a_str)
+# print(a_float)
+# print(type(a_float))
+
+# # does work
+# a_str = "4.5"
+# a_float = float(a_str)
+# print(a_float)
+# print(type(a_float))
+
+# str to boolean
+# # true
+# a_string = "hello"
+# a_bool = bool(a_string)
+# print(a_bool)
+# print(type(a_bool))
+
+# # empty string produces
+# a_string = "hello"
+# a_bool = bool(a_string)
+# print(a_bool)
+# print(type(a_bool))
+
+# int to str
+# an_int = 4
+# a_str = str(an_int)
+# print(a_str)
+# print(type(a_str))
+
+# int to int
+# an_int = 4
+# an_int = int(an_int)
+# print(an_int)
+# print(type(an_int))
+
+# int to float
+# an_int = 4
+# a_float = float(an_int)
+# print(a_float)
+# print(type(a_float))
+
+# int to bool
+# # 0 goes to False
+# an_int = 0
+# a_bool = bool(an_int)
+# print(a_bool)
+# print(type(a_bool))
+# # 1 goes to True
+# an_int = 1
+# a_bool = bool(an_int)
+# print(a_bool)
+# print(type(a_bool))
+# # Anything not 0 goes to True
+# an_int = 5
+# a_bool = bool(an_int)
+# print(a_bool)
+# print(type(a_bool))
+# an_int = -5
+# a_bool = bool(an_int)
+# print(a_bool)
+# print(type(a_bool))
+
+# float to string
+# a_float = -5.5
+# a_str = str(a_float)
+# print(a_str)
+# print(type(a_str))
+
+# float to int
+# # truncates it, NO ROUNDING
+# a_float = -5.8
+# an_int = int(a_float)
+# print(an_int)
+# print(type(an_int))
+
+# float to float
+# a_float = -5.8
+# a_float = float(a_float)
+# print(a_float)
+# print(type(a_float))
+
+# float to boolean
+# # Anything not 0.0 is True
+# a_float = -5.8
+# a_bool = bool(a_float)
+# print(a_bool)
+# print(type(a_bool))
+# # 0.0 is False
+# a_float = 0.0
+# a_bool = bool(a_float)
+# print(a_bool)
+# print(type(a_bool))
+# # 1 is True
+# a_float = 1.0
+# a_bool = bool(a_float)
+# print(a_bool)
+# print(type(a_bool))
+
+# bool to string
+# a_bool = True
+# a_str = str(a_bool)
+# print(a_str)
+# print(type(a_str))
+
+# bool to int
+# # True goes to 1
+# a_bool = True
+# an_int = int(a_bool)
+# print(an_int)
+# print(type(an_int))
+# # False goes to 0
+# a_bool = False
+# an_int = int(a_bool)
+# print(an_int)
+# print(type(an_int))
+
+# bool to float
+# # True goes to 1
+# a_bool = True
+# a_float = float(a_bool)
+# print(a_float)
+# print(type(a_float))
+# # False goes to 0
+# a_bool = False
+# a_float = float(a_bool)
+# print(a_float)
+# print(type(a_float))
+
+# bool to bool
+# a_bool = False
+# a_bool = bool(a_bool)
+# print(a_bool)
+# print(type(a_bool))
+
+# none to string
+# a_none = None
+# a_str = str(a_none)
+# print(a_str)
+# print(type(a_str))
+
+# none to int
+# # this will fail!
+# a_none = None
+# an_int = int(a_none)
+# print(an_int)
+# print(type(an_int))
+
+# none to float
+# a_none = None
+# a_float = float(a_none)
+# print(a_float)
+# print(type(a_float))
+
+# none to bool
+# a_none = None
+# a_bool = bool(a_none)
+# print(a_bool)
+# print(type(a_bool))
