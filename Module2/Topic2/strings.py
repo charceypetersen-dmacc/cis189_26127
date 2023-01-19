442d448d43c94607d623b715a7b0f9a1aeedc361
charceypetersen-dmacc <122238082+charceypetersen-dmacc@users.noreply.github.com>
2023-01-19 03:27:56 -0600

    Create strings.py

---
+++ Module2/Topic2/strings.py
@@ -0,0 +1,40 @@
+# statement = "Python is fun!"
+
+# concatenate
+# statement = statement + " But it is hard." + " But I am learning." + statement + statement
+# print(statement)
+
+# # produces 77 because two string concatenated just get smooshed together
+# x = "7"
+# y = "7"
+# print(x+y)
+
+# multiply
+# print(statement * 4)
+# # order of operations
+# print((statement + " ") * 4)
+
+# len
+# print(len(statement))
+# length = len("python")
+# print(length)
+
+# replace
+# statement = "Python is fun!"
+# statement = statement.replace("fun", "difficult")
+# print(statement)
+
+# if it doesn't find it, nothing happens
+# statement = "Python is fun!"
+# statement = statement.replace("hard", "difficult")
+# print(statement)
+
+# swap case
+# statement = "Python is fun!"
+# statement = statement.swapcase()
+# print(statement)
+
+# capitalize
+# statement = "python is fun!"
+# statement = statement.capitalize()
+# print(statement)
