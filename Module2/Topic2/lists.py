0ff877fd7c45f777c3509abb265bda2ddb06570a
charceypetersen-dmacc <122238082+charceypetersen-dmacc@users.noreply.github.com>
2023-01-19 03:27:40 -0600

    Create lists.py

---
+++ Module2/Topic2/lists.py
@@ -0,0 +1,59 @@
+# declare
+# a_list = []
+# print(a_list)
+# print(len(a_list))
+
+# a_list = ["Python", 4, 4.5, None, True]
+# print(a_list)
+# print(len(a_list))
+
+# order maintained
+# a_list = ["Python", 4, 4.5, None, True]
+# print(a_list)
+# # when the list is printed, it is in the order we created the items in
+# print(len(a_list))
+
+# duplicate items
+# a_list = ["Python", 4, 4.5, None, True, "Python"]
+# print(a_list)
+# print(len(a_list))
+
+# indexing
+# a_list = ["Python", 4, 4.5, None, True]
+# print(a_list)
+# print(len(a_list))
+# # computers start counting at 0!
+# print(a_list[0])
+# print(a_list[1])
+# print(a_list[2])
+# print(a_list[3])
+# print(a_list[4])
+# # print(a_list[5])  # this line will fail, index out of range
+#
+# index_1 = a_list[1]
+# print(index_1)
+
+# append
+# the item will be added, no reassignment necessary, lists are mutable!!!
+# a_list = ["Python", 4, 4.5, None, True]
+# a_list.append("Charcey")
+# print(a_list)
+# a_list.append("Petersen")
+# # a_list.append("Charcey", "Petersen")  # bad will not work!
+# print(a_list)
+
+# remove
+# a_list = ['Python', 4, 4.5, None, True, 'Charcey', 'Petersen']
+# print(a_list)
+# a_list.remove("Charcey")
+# print(a_list)
+# # a_list.remove("Charcey")  # will error! cannot remove if not in list
+# # print(a_list)
+
+# index
+# a_list = ['Python', 4, 4.5, None, True, 'Charcey', 'Petersen']
+# pos = a_list.index("Charcey")
+# print(pos)
+# print(a_list[5])
+# # pos = a_list.index("DMACC")  # error! cannot find index of something not in list!
+
